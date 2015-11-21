72a7612f6cf1793d44b196d63f63ceaaf2528922
csbok <winmain@gmail.com>
2015-11-21 13:47:55 +0900

    TopBar - ES2015

--- src/app/components/TopBar.jsx
+++ src/app/components/TopBar.jsx
@@ -1,37 +1,44 @@
 'use strict';
 
-const React = require('react');
-const jquery = require('jquery');
+import React 		from 'react';
+import jquery 		from 'jquery';
 
-const Colors = require('material-ui/lib/styles/colors');
-const IconButton = require('material-ui/lib/icon-button');
-const Avatar = require('material-ui/lib/avatar');
-const FlatButton = require('material-ui/lib/flat-button');
+import Colors 		from 'material-ui/lib/styles/colors';
+import IconButton 	from 'material-ui/lib/icon-button';
+import Avatar 		from 'material-ui/lib/avatar';
+import FlatButton 	from 'material-ui/lib/flat-button';
 
-const LoginDialog = require('./LoginDialog.jsx');
-const JoinDialog = require('./JoinDialog.jsx');
-const MainSnackBar = require('./MainSnackBar.jsx');
+import LoginDialog 	from './LoginDialog.jsx';
+import JoinDialog 	from './JoinDialog.jsx';
+import MainSnackBar from './MainSnackBar.jsx';
 
-const userStore = require('../store/userStore');
-const config = require('./config.js');
+import userStore 	from '../store/userStore';
+import config 		from './config.js';
 
 import * as UserActions from '../actions/user';
 import { bindActionCreators } from 'redux';
+//------------------------------------------------------------------------------------------------------------------
+
 const actions = bindActionCreators(UserActions, userStore.dispatch);
 
-const TopBar = React.createClass({
-	getInitialState: function() {
+export default class TopBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.login 				= this.login.bind(this);
+		this.homeButtonClick 	= this.homeButtonClick.bind(this);
+		this.logoutButtonClick 	= this.logoutButtonClick.bind(this);
+
 		this.login();
-
 		userStore.subscribe(this.onLogin.bind(this));
-		return {user: userStore.getState()};
-	},
 
-	homeButtonClick: function(e) {
+		this.state = {user: userStore.getState()};
+	}
+
+	homeButtonClick(e) {
 		e.preventDefault();
-	},
+	}
 
-	logoutButtonClick: function(e) {
+	logoutButtonClick(e) {
 		e.preventDefault();
 
 		jquery.support.cors = true;
@@ -59,10 +66,10 @@ const TopBar = React.createClass({
 					global.mainSnackbar.show();
 			}.bind(this),
 		});
-	},
+	}
 
 	// url을 직접 입력하여 이동되었거나, F5로 새로고침 하였을때, 로그인이 되었는지 확인
-	login: function() {
+	login() {
 		jquery.support.cors = true;
 		jquery.ajax({
 			xhrFields: {
@@ -92,87 +99,86 @@ const TopBar = React.createClass({
 				this.refs.snackbar.show();
 			}.bind(this),
 		});		
-	},
+	}
 
 	// redux로 부터 로그인 상태가 변경되면 호출됨
-	onLogin: function() {
+	onLogin() {
 		this.setState({user: userStore.getState()});
-	},
+	}
 
 
 
-	render: function() {
- 	let styles = {
-		iconButton: {
-			position: 'absolute',
-			left: 0,
-			backgroundColor: Colors.brown500,//deepOrange500,
-			color: 'white',
-		},
-		iconStyle: {
-			color: Colors.white,
-		},
-		avatar: {
-			position: 'absolute',
-			top: 4,
-			left: 40,
-		},      
-		loginButton: {
-			position: 'absolute',
-			top: 6,
-			left: 80,
-			backgroundColor: Colors.brown500,//deepOrange500,
-			color: 'white',
-		},
-		joinButton: {
-			position: 'absolute',
-			top: 6,
-			left: 160,
-			backgroundColor: Colors.brown500,//deepOrange500,
-			color: 'white',
-		},
-		fiexedBarStyle: {
-			position: 'fixed',
-			marginTop:0,
-			marginLeft: 0,
-			backgroundColor: Colors.brown500,//deepOrange500,
-			top:0,
-			width: '100%',
-			height: 48,
-			zIndex:9998,
-		},		
-	};
+	render() {
+	 	let styles = {
+			iconButton: {
+				position: 'absolute',
+				left: 0,
+				backgroundColor: Colors.brown500,//deepOrange500,
+				color: 'white',
+			},
+			iconStyle: {
+				color: Colors.white,
+			},
+			avatar: {
+				position: 'absolute',
+				top: 4,
+				left: 40,
+			},      
+			loginButton: {
+				position: 'absolute',
+				top: 6,
+				left: 80,
+				backgroundColor: Colors.brown500,//deepOrange500,
+				color: 'white',
+			},
+			joinButton: {
+				position: 'absolute',
+				top: 6,
+				left: 160,
+				backgroundColor: Colors.brown500,//deepOrange500,
+				color: 'white',
+			},
+			fiexedBarStyle: {
+				position: 'fixed',
+				marginTop:0,
+				marginLeft: 0,
+				backgroundColor: Colors.brown500,//deepOrange500,
+				top:0,
+				width: '100%',
+				height: 48,
+				zIndex:9998,
+			},		
+		};
 
-    return (
-    	<div>
-    		<div style={styles.fiexedBarStyle}>
-		    	<IconButton
-					onClick={this.homeButtonClick.bind(null, this)}
-					iconClassName="material-icons"
-					style={styles.iconButton}
-					iconStyle={styles.iconStyle}>
-					home
-				</IconButton>
-				{this.state.user.isLogin ? 
-					<div>
-						<Avatar src="http://lorempixel.com/100/100/nature/" style={styles.avatar} />
-						<FlatButton label="로그아웃"  onTouchTap={this.logoutButtonClick}  style={styles.loginButton} />
-					</div>
-				:
-					<div>
-						<FlatButton label="로그인"  onTouchTap={()=>{global.loginDialog.show();}}  style={styles.loginButton} />
-						<FlatButton label="회원가입"  onTouchTap={()=>{global.joinDialog.show();}}  style={styles.joinButton} />
-					</div>
-				}
+	    return (
+	    	<div>
+	    		<div style={styles.fiexedBarStyle}>
+			    	<IconButton
+						onClick={this.homeButtonClick.bind(null, this)}
+						iconClassName="material-icons"
+						style={styles.iconButton}
+						iconStyle={styles.iconStyle}>
+						home
+					</IconButton>
+					{this.state.user.isLogin ? 
+						<div>
+							<Avatar src="http://lorempixel.com/100/100/nature/" style={styles.avatar} />
+							<FlatButton label="로그아웃"  onTouchTap={this.logoutButtonClick}  style={styles.loginButton} />
+						</div>
+					:
+						<div>
+							<FlatButton label="로그인"  onTouchTap={()=>{global.loginDialog.show();}}  style={styles.loginButton} />
+							<FlatButton label="회원가입"  onTouchTap={()=>{global.joinDialog.show();}}  style={styles.joinButton} />
+						</div>
+					}
+				</div>
+
+				<LoginDialog />
+				<JoinDialog />
+				<MainSnackBar />
+
 			</div>
+			)
+    }
+}
 
-			<LoginDialog />
-			<JoinDialog />
-			<MainSnackBar />
-
-		</div>
-		)
-    },
-});
-
-module.exports = TopBar;
