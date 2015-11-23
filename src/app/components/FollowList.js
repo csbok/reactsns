e980a44c7e8ab99e88b4689f0d5f0b23164b87c5
csbok <winmain@gmail.com>
2015-11-23 17:00:09 +0900

    insert a follow button to FollowDialog

--- src/app/components/FollowList.js
+++ src/app/components/FollowList.js
@@ -2,6 +2,7 @@
 
 const React = require('react');
 const Dialog = require('material-ui/lib/dialog');
+const FollowButton = require('./FollowButton.jsx');
 
 const FollowList = React.createClass({
   getInitialState : function() {
@@ -16,7 +17,10 @@ const FollowList = React.createClass({
 	render: function() {
   const commentNodes = this.state.listdata.map(function(item) {
     return(
-      <div key={item.user_no} style={{marginLeft:'5px'}}><div style={{display:'inline-block',width:'100px'}}>{item.user_name}</div> follow</div>
+      <div key={item.user_no} style={{marginLeft:'5px'}}>
+        <div style={{display:'inline-block',width:'100px'}}>{item.user_name}</div>
+        <FollowButton user_no={item.user_no} already={item.follow_already} />
+       </div>
     );});
 
 		return (
