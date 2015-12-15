e3aaac033da793a9cdd2cfb41bea9f2b51c2d5a3
csbok <winmain@gmail.com>
2015-12-15 18:02:17 +0900

    tab menus are changed when user login status

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -6,15 +6,23 @@ import Colors   from 'material-ui/lib/styles/colors';
 import Tabs     from 'material-ui/lib/tabs/tabs';
 import Tab      from 'material-ui/lib/tabs/tab';
 import TopBar   from './TopBar.jsx';
+import userStore 	from '../store/userStore';
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {tabIndex : 0};
+		userStore.subscribe(this.onLogin.bind(this));
+    this.state = {tabIndex : 0, user: userStore.getState()};
 
     this._handleTabChange = this._handleTabChange.bind(this);
   }
+  
+	// redux로 부터 로그인 상태가 변경되면 호출됨
+	onLogin() {
+		this.setState({user: userStore.getState()});
+	}
+
 
   _handleTabChange(value, e, tab) {
     if (!isNaN(value)) {
@@ -48,7 +56,8 @@ export default class Main extends React.Component {
     return (
       <div>
         <TopBar />
-           <div>
+        <div>
+        {this.state.user.isLogin ? 
               <Tabs
               value={this.state.tabIndex}
               style={styles.tabsContainer}
@@ -56,13 +65,28 @@ export default class Main extends React.Component {
                 contentContainerStyle={styles.contentContainerStyle}>
                 <Tab label="새로운 글" value="1" route="/" style={styles.tab}>
                 </Tab>
+
                 <Tab label="내 정보" value="2" route="/myinfo" style={styles.tab}>
                 </Tab>
+                 
                 <Tab label="타임라인" value="3" route="/timeline"  style={styles.tab}>
                 </Tab>
+                 
+              </Tabs>
+          : 
+              <Tabs
+              value={this.state.tabIndex}
+              style={styles.tabsContainer}
+              onChange={this._handleTabChange}
+                contentContainerStyle={styles.contentContainerStyle}>
+                <Tab label="새로운 글" value="1" route="/" style={styles.tab}>
+                </Tab>
+
                 <Tab label="로그인" value="4" route="/login" style={styles.tab}>
                 </Tab>
+                                 
               </Tabs>
+          }
           </div>
           <div style={styles.content}>{this.props.children}</div>
 
